cb8ed4f6351a6b5377d7133f52e958956cd5c316
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-08-16 13:21:24 -0700

    Added basic structuring elements and broke down lab instructions to apply to each file.

---
+++ js/app.js
@@ -0,0 +1,9 @@
+'use strict'
+
+console.log('do re me fa so la ti do');
+
+// What are we going to display?
+// Cities
+// Min & Max Customer & Avg Cookie/Sale from 6am-7pm (13 hours)
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random - use Math.random() for the formulas
+// calculate the min/max cust and avg cookie and have them display in an unordered list on the sales.html
