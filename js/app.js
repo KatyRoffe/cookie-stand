e8f6bd90a3f999923eb7a276e1592755bd6472c3
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-08-16 21:38:09 -0700

    Improves objects properties and methods.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 'use strict'
 
-console.log('do re me fa so la ti do');
+// console.log('do re me fa so la ti do');
 
 // What are we going to display?
 // Min & Max Customer & Avg Cookie/Sale from 6am-7pm (13 hours)
@@ -9,50 +9,108 @@ console.log('do re me fa so la ti do');
 
 // set up object literals for store locations
 
-const salmonDiv = document.getElementById('salmonCookies');
+// var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
+
+// const salmonDiv = document.getElementById('salmonCookies');
 
 const seattle = {
   location: 'Seattle',
-  avgCustomer: null,
-  avgSale: 6.3,
-  getCustomer: function () {
-    this.avgCustomer = `${_randCust(23, 65)} customers`;
+  minCust: 23,
+  maxCust: 65,
+  avgCookieSale: 6.3,
+  randomCustPerHour: function(min, max) {
+    min = Math.ceil(min);
+    min = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+
+  dailySales: function () {
+    let randomCust = this.randomCustPerHour(this.minCust,this.maxCust)
+    let hourlyCookieTotal = randomCust*this.avgCookieSale
+    hourlyCookieTotal = Math.round(hourlyCookieTotal)
+    console.log(hourlyCookieTotal)
   }
 }
+seattle.dailySales()
 
 const tokyo = {
   location: 'Tokyo',
-  avgCustomer: null,
-  avgSale: 1.2,
-  getCustomer: function() {
-    this.avgCustomer = `${_randCust(3, 24)} customers`;
+  minCust: 3,
+  maxCust: 24,
+  avgCookieSale: 1.2,
+  randomCustPerHour: function(min, max) {
+    min = Math.ceil(min);
+    min = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+
+  dailySales: function () {
+    let randomCust = this.randomCustPerHour(this.minCust,this.maxCust)
+    let hourlyCookieTotal = randomCust*this.avgCookieSale
+    hourlyCookieTotal = Math.round(hourlyCookieTotal)
+    console.log(hourlyCookieTotal)
   }
 }
+tokyo.dailySales()
 
 const dubai = {
   location: 'Dubai',
-  avgCustomer: null,
-  avgSale: 3.7,
-  getCustomer: function () {
-    this.avgCustomer = `${_randCust(11, 38)} customers`;
+  minCust: 11,
+  maxCust: 38,
+  avgCookieSale: 3.7,
+  randomCustPerHour: function(min, max) {
+    min = Math.ceil(min);
+    min = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+
+  dailySales: function () {
+    let randomCust = this.randomCustPerHour(this.minCust,this.maxCust)
+    let hourlyCookieTotal = randomCust*this.avgCookieSale
+    hourlyCookieTotal = Math.round(hourlyCookieTotal)
+    console.log(hourlyCookieTotal)
   }
 }
+dubai.dailySales()
 
 const paris = {
-  location: 'Paris',
-  avgCustomer: null,
-  avgSale:2.3,
-  getCustomer: function () {
-    this.avgCustomer = `${_randCust(20, 38)} customers`;
+  location: 'Paros',
+  minCust: 20,
+  maxCust: 38,
+  avgCookieSale: 2.3,
+  randomCustPerHour: function(min, max) {
+    min = Math.ceil(min);
+    min = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+
+  dailySales: function () {
+    let randomCust = this.randomCustPerHour(this.minCust,this.maxCust)
+    let hourlyCookieTotal = randomCust*this.avgCookieSale
+    hourlyCookieTotal = Math.round(hourlyCookieTotal)
+    console.log(hourlyCookieTotal)
   }
 }
+paris.dailySales()
 
 const lima = {
   location: 'Lima',
-  avgCustomer: null,
-  avgSale: 4.6,
-  getCustomer: function () {
-    this.avgCustomer = `${_randCust(20, 38)} customers`;
+  minCust: 2,
+  maxCust: 16,
+  avgCookieSale: 4.6,
+  randomCustPerHour: function(min, max) {
+    min = Math.ceil(min);
+    min = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+
+  dailySales: function () {
+    let randomCust = this.randomCustPerHour(this.minCust,this.maxCust)
+    let hourlyCookieTotal = randomCust*this.avgCookieSale
+    hourlyCookieTotal = Math.round(hourlyCookieTotal)
+    console.log(hourlyCookieTotal)
   }
 }
+
+lima.dailySales()
 
