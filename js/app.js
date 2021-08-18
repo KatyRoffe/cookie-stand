aaaddd85dd718b7c55605649a8a79693a4077a6a
KATYROFFE <KATYROFFE@GMAIL.COM>
2021-08-17 23:53:39 -0700

    Begins transitioning objects to constructor and setting up a table.

--- js/app.js
+++ js/app.js
@@ -3,8 +3,42 @@
 
 const locationsDiv = document.getElementById('location');
 
+
+//hours of operations
 const hoursOfOperation = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 
+
+function Store(location, minCust, maxCust, avgSalePerCust) {
+  this.locationVal = location;
+  this.minCustVal = minCust;
+  this.macCustVal = maxCust;
+  this.avgSalePerCustVal = avgSalePerCust;
+  this.hrlySalesArrayVal = []; 
+
+ Store.locationsArray.push(this); // not sure if this is right
+}
+// not sure if any of the following is correct or workable...
+Store.locationArray = [];
+Store.prototype.cookieSales = function() {
+  let dailySales = 0;
+  let hourlySales = [];
+  for (let i = 0; i < this.hoursOfOperation.length; i++) {
+    let randomNumCustPerHour = Math.floor(Math.random() * (this.maxCustomer - this.minCustomer) + this.minCustomer);
+    hourlySales.push(Math.ceil(randomNumberOfCustomers * this.avgSalePerCust)); 
+    dailySales += (Math.ceil(randomNumberOfCustomers * this.avgSalePerCust));
+  }
+  this.salesPerHour = hourlySales;
+  
+}
+
+//pretty certain about these
+new Store('Seattle', 23, 65, 6.3)
+new Store('Tokyo', 3, 24, 1.2)
+new Store('Dubai', 11, 38, 3.7)
+new Store('Paris', 20, 38, 2.3)
+new Store('Lima', 2, 16, 4.6)
+
+console.log(Store.randomNumCustPerHour)
 
 //seattle
 const seattle = {
@@ -20,7 +54,7 @@ const seattle = {
   fillHrlySalesArray: function() {
     for (let i = 0; i < hoursOfOperation.length; i++) {
       let salesPerHour = this.randomNumCustPerHour() * this.avgSalePerCust;
-      this.hrlySalesArray.push(Math.ceil(salesPerHour));
+      this.hrlySalesArray.push(Math.ceil(this.avgSalePerCust));
     }
   }
 }
@@ -124,6 +158,35 @@ function renderLocation(location) {
   _makeElement('li', ulElem, totalString);
 }
 
+
+function makeTable() {
+  let body = document.getElementById('locations');
+  let tableElem = document.createElement('table');
+  let tableBody = document.createElement('tbody');
+  for(let i=0; i< Location.appendChild.locationsArray.length; i++) {
+    let currentLocation = Location.locationArray[i];
+    let rowElem = document.createElement('tr');
+    tableBody.appendChild(rowElem);
+    let tdElem = document.createElement('td');
+    let cityCell = currentLocation.location;
+    tdElem.textContent = cityCell;
+    rowElem.appendChild(tdElem);
+    for(let j = 0; j < currentLocation.hoursOfOperation.length; j++) {
+      let cellElem = document.createElement('td');
+      cellElem.textContent = currentLocation.salesPerHour[j];
+      rowElem.appendChild(cellElem);
+    }
+    tableBody.appendChild(rowElem);
+  }
+  tableElem.appendChild(tableBody);
+  body.appendChild(tableElem);
+  tableElem.setAttribute("border", "3");
+}
+makeTable();
+
+
+
+
 const locationsArray = [seattle, tokyo, dubai, paris, lima];
 
 function renderAllLocations() {
@@ -135,4 +198,3 @@ function renderAllLocations() {
 }
 
 renderAllLocations();
-
